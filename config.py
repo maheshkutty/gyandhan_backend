b4b70ec889c1e7b898a4523b6c859ffa006ca65a
Mahesh Kutty <bosskutty17@gmail.com>
2022-01-27 23:29:12 +0530

    last commit

--- config.py
+++
@@ -1,5 +0,0 @@
-MYSQL_HOST = 'bydiz8r778bzwki9m9qz-mysql.services.clever-cloud.com'
-MYSQL_USER = 'ublkq00bapxcfitc'
-MYSQL_PASSWORD = 'wgiH8ZVRtpz4oG4LRI28'
-MYSQL_DB = 'bydiz8r778bzwki9m9qz'
-MYSQL_PORT = 3306
